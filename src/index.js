381298fc9ef7582cb25e55e75e7716c67e3b14e3
Roman Popov <djrulik@gmail.com>
2023-08-08 02:05:51 +0300

    init: initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
+//function = () =>{
 
+
+
+
+
+
+//}
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    
+let result = [];
+    for (let i in matrix) {
+        if (Array.isArray(matrix[i])) {
+            let alt = (matrix[i]);
+            for (let j in alt){
+                result.push(alt[j]);
+            }
+        } else {                
+        result.push(matrix[i]);
+        }
+
+    }
+console.log(result);
+
+
+
+  return result;
 }
